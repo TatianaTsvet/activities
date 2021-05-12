d236b41f55364f6fb10ac3d49e8b729e6edcf192
Цветынина Татьяна Анатольевна <tcvetynina@milane.local>
2021-05-12 09:22:24 +0300

    with spinner

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+        <div className="flex mx-auto loadingio-spinner-spinner-ugqminf5j9a">
+            <div className="ldio-lbb5d507iik"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Spinner;
