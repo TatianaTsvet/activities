ebb29379418df112343cb2ad28bf476dae77eef1
Цветынина Татьяна <tcvetynina@milane.local>
2021-06-28 10:39:19 +0300

    fixed problems with intersection observer in IE

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import "babel-polyfill";
 import "react-app-polyfill/ie11";
 import "react-app-polyfill/stable";
+import "intersection-observer";
+import "intersection-observer-polyfill";
 import "core-js/features/string/repeat";
 import "core-js/es/map";
 import "core-js/es/set";
 import "raf/polyfill";
+import "abortcontroller-polyfill";
+
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/";
 import { Provider } from "react-redux";
-import "intersection-observer-polyfill";
+
 import store from "./saga/store/store";
 
 import "./index.scss";
