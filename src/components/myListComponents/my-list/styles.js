5936eb053842be03eeae011740507c03fd0fed9d
Цветынина Татьяна <tcvetynina@milane.local>
2021-07-19 13:42:05 +0300

    added styles for backToTop button

--- src/components/myListComponents/my-list/styles.js
+++ src/components/myListComponents/my-list/styles.js
@@ -2,7 +2,8 @@ const styles = (theme) => ({
   backToTop: {
     backgroundColor: "#1976d2",
     position: "fixed",
-    bottom: "0.5em",
+    bottom: "0.1em",
+    left: "1em",
   },
 });
 export default styles;
